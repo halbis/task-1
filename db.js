61715324d09661e52a34d60b68b33587feb03c23
b3hal9 <b3hal9@gmail.com>
2022-03-26 13:23:45 +0545

    database config file

---
+++ db.js
@@ -0,0 +1,16 @@
+const sqlite3 = require("sqlite3").verbose();
+const logger = require("./utils/logger");
+
+const db = new sqlite3.Database(
+  "./db/test.db",
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) {
+      logger.error(err.message, err.stack);
+      return;
+    }
+    logger.info("database connected..");
+  }
+);
+
+module.exports = db;
